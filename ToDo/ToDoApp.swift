1b06b86a47ec4062dab934a912159172dd3a5ee5
KikiSpace <xzhangkiki@gmail.com>
2024-02-08 12:21:57 -0800

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Xinqi Zhang on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
